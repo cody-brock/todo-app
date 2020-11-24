6c1a3ccac6a4ed9608f74823ce83fc2ac74b10d1
eternal golden bread <codylewisbrock@gmail.com>
2020-11-23 17:43:53 -0800

    1. - Adds bootstrap and dependencies into react-ui package.json.  2 - Decided that I will first get users and authentication working for this app, so begins work on a login page.  3 - Adds a separate folder for reusable custom hooks, and adds the first one for handling sign ups.

--- react-ui/src/components/App/index.js
+++ react-ui/src/components/App/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import Login from '../Login/index.js'
 import './style.css';
 
--- react-ui/src/components/Login/index.js
+++ react-ui/src/components/Login/index.js
@@ -1,64 +1,112 @@
 import React, { useCallback, useEffect, useState } from 'react';
+
+import useSignUpForm from '../../hooks/useSignUpForm/index';
+
+
 // import './App.css';
 
 function Login() {
-  const [message, setMessage] = useState(null);
-  const [isFetching, setIsFetching] = useState(false);
-  const [url, setUrl] = useState('/api');
+  const [username, setUsername] = useState("blah");
 
-  const fetchData = useCallback(() => {
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        setMessage(json.message);
-        setIsFetching(false);
-      }).catch(e => {
-        setMessage(`API call failed: ${e}`);
-        setIsFetching(false);
-      })
-  }, [url]);
 
-  useEffect(() => {
-    setIsFetching(true);
-    fetchData();
-  }, [fetchData]);
+  const signup = () => {
+    alert(`User created! Username: ${inputs.username}`);
+  }
+
+  const {inputs, handleInputChange, handleSubmit} = useSignUpForm(signup);
+
+
+  // const handleSubmit = (evt) => {
+  //   evt.preventDefault();
+  //   alert(`Your username is ${username}`);
+  // }
+
+  // const [message, setMessage] = useState(null);
+  // const [isFetching, setIsFetching] = useState(false);
+  // const [url, setUrl] = useState('/api');
+
+  // const fetchData = useCallback(() => {
+  //   fetch(url)
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error(`status ${response.status}`);
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(json => {
+  //       setMessage(json.message);
+  //       setIsFetching(false);
+  //     }).catch(e => {
+  //       setMessage(`API call failed: ${e}`);
+  //       setIsFetching(false);
+  //     })
+  // }, [url]);
+
+  // useEffect(() => {
+  //   setIsFetching(true);
+  //   fetchData();
+  // }, [fetchData]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        { process.env.NODE_ENV === 'production' ?
-            <p>
-              This is a production build from create-react-app.
-            </p>
-          : <p>
-              blah blah blah
-            </p>
-        }
-        <p>{'« '}<strong>
-          {isFetching
-            ? 'Fetching message from API'
-            : message}
-        </strong>{' »'}</p>
-        <p><a
-          className="App-link"
-          href="https://github.com/mars/heroku-cra-node"
-        >
-          React + Node deployment on Heroku
-        </a></p>
-        <p><a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a></p>
-      </header>
+      <div>
+        Login time
+      </div>
+      <form onSubmit={handleSubmit}>
+          <div className="form-group">
+
+            <div className="row">
+              <div className="col-sm-12">
+                <div className="card login-label">
+
+                  <label htmlFor="username">Username</label>
+                  <input 
+                    type="text" 
+                    name="username"
+                    value={inputs.username}
+                    onChange={handleInputChange} 
+                    className="form-control login-form" 
+                    placeholder="Enter Username Here" 
+                  />
+
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="row">
+              <div className="col-sm-12">
+                <div className="card login-label">
+                  <label htmlFor="password">Password</label>
+                  <input 
+                    type="password" 
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleChange} 
+                    className="form-control login-form" 
+                    placeholder="Enter Password Here"
+                  />
+                </div>
+              </div>
+            </div> */}
+
+            <div className="row">
+              <div className="col-sm-12">
+                <button
+                  className="btn btn-signup"
+                  type="submit"
+                  // disabled={this.state.loading}
+                  // onClick={this.handleLogIn}
+                  >
+                  {/* {this.state.loading && (
+                    <span className="spinner-border spinner-border-sm"></span>
+                    )} */}
+                  <span> Log In</span>
+                </button>
+              </div>
+            </div>
+
+          </div>
+        </form>
     </div>
   );
 
