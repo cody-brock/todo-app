b14de846117f16aad605939e503d12e7a4604694
eternal golden bread <codylewisbrock@gmail.com>
2020-11-25 14:58:02 -0800

    working on the react-ui section of the app.  Not currently working, need to rewire some things.  Saving at a midpoint.

---
+++ react-ui/src/App/index.js
@@ -0,0 +1,84 @@
+// import React from 'react';
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// import Login from '../Login/index.js'
+// import Signup from '../Signup/index.js'
+// import './style.css';
+
+// function App() {
+
+
+//   return (
+//     <div>
+//       <Login />
+//       <Signup />
+
+//     </div>
+    
+//   );
+
+// }
+
+// export default App;
+
+
+
+
+
+
+
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { history } from '../_helpers';
+import { alertActions } from '../_actions';
+import { PrivateRoute } from '../_components';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './style.css';
+import Login from '../LoginPage/LoginPage.js'
+import Signup from '../Signup/index.js'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const { dispatch } = this.props;
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear());
+        });
+    }
+
+    render() {
+        const { alert } = this.props;
+        return (
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        {alert.message &&
+                            <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        }
+                        <Router history={history}>
+                            <div>
+                                <PrivateRoute exact path="/" component={HomePage} />
+                                <Route path="/login" component={LoginPage} />
+                            </div>
+                        </Router>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { alert } = state;
+    return {
+        alert
+    };
+}
+
+const connectedApp = connect(mapStateToProps)(App);
+export { connectedApp as App };
---
+++ react-ui/src/LoginPage/LoginPage.js
@@ -0,0 +1,160 @@
+// import React, { useCallback, useEffect, useState } from 'react';
+
+// import useSignUpForm from '../hooks/useSignUpForm/index';
+
+
+// function Login() {
+
+//   // Callback which is passed to useSignUpForm
+//   const signup = () => {
+//     alert(`Login Received! Username: ${inputs.username}`);
+//   }
+
+//   // Destructuring for clarity
+//   const {inputs, handleInputChange, handleSubmit} = useSignUpForm(signup);
+
+//   return (
+//     <div className="App">
+//       <div>
+//         Login time
+//       </div>
+//       <form onSubmit={handleSubmit}>
+//           <div className="form-group">
+
+//             <div className="row">
+//               <div className="col-sm-12">
+//                 <div className="card login-label">
+
+//                   <label htmlFor="username">Username</label>
+//                   <input 
+//                     type="text" 
+//                     name="username"
+//                     value={inputs.username || ""}
+//                     onChange={handleInputChange} 
+//                     className="form-control login-form" 
+//                     placeholder="Enter Username Here" 
+//                   />
+
+//                 </div>
+//               </div>
+//             </div>
+
+//             <div className="row">
+//               <div className="col-sm-12">
+//                 <div className="card login-label">
+
+//                   <label htmlFor="password">Password</label>
+//                   <input 
+//                     type="password" 
+//                     name="password"
+//                     value={inputs.password || ""}
+//                     onChange={handleInputChange} 
+//                     className="form-control login-form" 
+//                     placeholder="Enter Password Here"
+//                   />
+
+//                 </div>
+//               </div>
+//             </div>
+
+//             <div className="row">
+//               <div className="col-sm-12">
+//                 <button
+//                   className="btn btn-signup"
+//                   type="submit"
+//                   >
+//                   <span> Log In</span>
+//                 </button>
+//               </div>
+//             </div>
+
+//           </div>
+//         </form>
+//     </div>
+//   );
+
+// }
+
+// export default Login;
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // reset login status
+        this.props.dispatch(userActions.logout());
+
+        this.state = {
+            username: '',
+            password: '',
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({ submitted: true });
+        const { username, password } = this.state;
+        const { dispatch } = this.props;
+        if (username && password) {
+            dispatch(userActions.login(username, password));
+        }
+    }
+
+    render() {
+        const { loggingIn } = this.props;
+        const { username, password, submitted } = this.state;
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h2>Login</h2>
+                <form name="form" onSubmit={this.handleSubmit}>
+                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
+                        {submitted && !username &&
+                            <div className="help-block">Username is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
+                        {submitted && !password &&
+                            <div className="help-block">Password is required</div>
+                        }
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary">Login</button>
+                        {loggingIn &&
+                            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                        }
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { loggingIn } = state.authentication;
+    return {
+        loggingIn
+    };
+}
+
+const connectedLoginPage = connect(mapStateToProps)(LoginPage);
+export { connectedLoginPage as LoginPage }; 
